655ba873f61b28cab302cbb9bda286a6bad36ee8
ntuan7059 <ntuannguyen9999@gmail.com>
2021-09-12 13:49:26 +0700

    finishregister

--- server.js
+++ server.js
@@ -1,12 +1,14 @@
 const express = require('express')
 const connectDB = require('./config/db')
-
+const cors = require('cors')
 const app= express()
 connectDB()
 
 //init middleware
 app.use(express.json({extended: false}))
-
+app.use(cors({
+    origin: "http://localhost:3000"
+}))
 app.get('/',(req, res)=> res.send('API running'))
 
 //define router
